fafb3ebfcd4a4081f9b7605e09d08fc0268f13f7
Alex <awd1467@gmail.com>
2017-10-09 18:04:57 -0400

    revert

--- src/base.js
+++ src/base.js
@@ -1,19 +1,13 @@
 import Rebase from 're-base';
 
 	var firebase = require('firebase');
-    var app =   try {
-    			firebase.initializeApp({
+    var app =   firebase.initializeApp({
 				apiKey: "AIzaSyDlfQpZcSLtxuuWp4jzMyy8D5Cr_eJOiyU",
 				authDomain: "goat-heard.firebaseapp.com",
 				databaseURL: "https://goat-heard.firebaseio.com",
 				storageBucket: "goat-heard.appspot.com",
 				messagingSenderId: "572663215225"
     			});
-			} catch (err) {
-				if (!/already exists/.test(err.message)) {
-					console.error('Firebase initialization error', err.stack)
-				}
-			}
 
     const base = Rebase.createClass(app.database());
 
